01ad679cd3893f6e02abe90baa0f9734dc5eebea
delian1986 <delian1914@gmail.com>
2019-06-09 21:30:04 +0300

    List of Items

--- 02. JS-Advanced-DOM-Manipulations-Resources/01. List-Of-Items/app.js
+++ 02. JS-Advanced-DOM-Manipulations-Resources/01. List-Of-Items/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputValue = document.getElementById('newItemText').value;
+    if (inputValue.length > 0) {
+        let ul = document.getElementById('items');
+
+        let newListItem = document.createElement('li');
+        ul.appendChild(newListItem);
+        newListItem.innerHTML = inputValue;
+
+        inputValue.value= '';
+    }
 }
