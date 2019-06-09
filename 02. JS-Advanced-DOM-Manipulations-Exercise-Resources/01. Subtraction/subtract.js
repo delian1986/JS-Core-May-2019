9e523df075b3c7536b62bead55b1ffb921fa62af
delian1986 <delian1914@gmail.com>
2019-06-09 23:05:53 +0300

    dom manipulations

---
+++ 02. JS-Advanced-DOM-Manipulations-Exercise-Resources/01. Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+    let num1= document.getElementById('firstNumber').value;
+    let num2= document.getElementById('secondNumber').value;
+    let result=num1-num2;
+
+    document.getElementById('result').innerHTML=result;
+}
--- 02. JS-Advanced-DOM-Manipulations-Resources/02. Add-Delete/app.js
+++ 02. JS-Advanced-DOM-Manipulations-Resources/02. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    let inputValue = document.getElementById('newText').value;
+
+    if (inputValue.length>0){
+        let newItem = document.createElement('li');
+        let ul = document.getElementById('items');
+        ul.appendChild(newItem);
+        newItem.innerHTML = inputValue.trim();
+
+        let a = document.createElement('a');
+        newItem.appendChild(a);
+        a.setAttribute('href','#');
+        a.innerHTML = '[Delete]';
+        a.addEventListener('click',removeItem);
+
+        function removeItem(){
+            let node = this.parentNode;
+            ul.removeChild(node);
+        }
+    }
 }
