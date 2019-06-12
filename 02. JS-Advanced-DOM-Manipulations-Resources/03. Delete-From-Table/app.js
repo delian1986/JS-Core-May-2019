967a03596b253440196afc6f8373006005440e0d
delian1986 <delian1914@gmail.com>
2019-06-12 22:47:28 +0300

    DOM exercises

--- 02. JS-Advanced-DOM-Manipulations-Resources/03. Delete-From-Table/app.js
+++ 02. JS-Advanced-DOM-Manipulations-Resources/03. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputText = document.getElementsByName('email')[0].value;
+    if(inputText){
+        let tableData = document.querySelectorAll('#customers td');
+        let result = document.getElementById('result');
+        
+        for (const data of tableData) {
+            if(data.innerHTML === inputText){
+                data.parentNode.parentNode.removeChild(data.parentNode);
+                result.innerHTML = 'Deleted.';
+                return;
+            }
+        }
+
+        result.innerHTML = 'Not found.';
+    }
 }
--- 02. JS-Advanced-DOM-Manipulations-Resources/04. Stopwatch/stopwatch.js
+++ 02. JS-Advanced-DOM-Manipulations-Resources/04. Stopwatch/stopwatch.js
@@ -1,3 +1,28 @@
 function stopwatch() {
-    console.log('TODO:...');
+    let startBtn = document.getElementById('startBtn');
+    let stopBtn = document.getElementById('stopBtn');
+    let timer = document.getElementById('time');
+
+    startBtn.addEventListener('click', startTimer);
+    stopBtn.addEventListener('click', stopTimer);
+
+
+    function startTimer() {
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+        started = true;
+
+        let count = 0;
+        setInterval(() => {
+                timer.innerHTML = count++;
+        }, 1000)
+
+    }
+
+    function stopTimer() {
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+        started = false;
+        timer.innerHTML = '00:00';
+    }
 }
